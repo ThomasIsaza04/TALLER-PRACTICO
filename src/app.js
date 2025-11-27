4e426528f9e75b788f42defc8839bc3d176dcf39
Thomas <thomasisazachalarca@gmail.com>
2025-11-27 09:39:29 -0500

    chore: configure cors and rate limiting

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express"
 import authRoutes from "./routes/auth.js";
 import tasksRoutes from "./routes/tasks.js"; 
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app = express()
 const PORT = 3000
@@ -15,6 +17,32 @@ app.use("/auth", authRoutes);
 
 
 app.use('/', tasksRoutes);
+
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN;
+
+app.use(
+  cors({
+    origin: FRONTEND_ORIGIN || "*", // ⚠️ En producción NO usar "*"
+  })
+);
+
+
+const authLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minuto
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use("/auth", authLimiter);
+
+
+const tasksLimiter = rateLimit({
+  windowMs: 60 * 1000, 
+  max: 60, 
+});
+
+app.use("/tasks", tasksLimiter);
 
 // Ruta de prueba
 
